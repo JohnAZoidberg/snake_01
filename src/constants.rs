c304d36014df25d32f3c7709f2d62f4d950d9587
Daniel Schaefer <dhs@frame.work>
2023-11-05 14:28:52 +0200

    Update to 34x9 grid for Framework 16 matrix
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,8 @@
 pub static NAME: &str = "Snake v01";
 
 // Board Dimensions
-pub const BOARD_WIDTH: u8 = 10;
-pub const BOARD_HEIGHT: u8 = 10;
+pub const BOARD_WIDTH: u8 = 9;
+pub const BOARD_HEIGHT: u8 = 34;
 
 // Colours
 pub const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
