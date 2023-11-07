ebc5cec4949623630e192ece1116dfc6e548c1d0
Daniel Schaefer <dhs@frame.work>
2023-11-07 13:58:32 +0300

    ledris: Remove snake code
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- src/ledris.rs
+++ src/ledris.rs
@@ -1,7 +1,6 @@
 extern crate rand;
 
 use rand::Rng;
-use std::collections::VecDeque;
 use std::fmt;
 
 use crate::constants::*;
@@ -25,7 +24,7 @@ impl Default for ExtendedGrid {
                 }
             }
         }
-        println!("Grid: {:?}", grid);
+        //println!("Grid: {:?}", grid);
         grid
     }
 }
@@ -37,7 +36,7 @@ impl ExtendedGrid {
             }
         }
     }
-    fn compact_rows(&mut self) -> u8 {
+    fn compact_rows(&mut self) -> u32 {
         let mut score = 0;
 
         // Check every row
@@ -49,7 +48,7 @@ impl ExtendedGrid {
                 }
             }
             if full_row {
-                println!("Row: {} is full", row);
+                //println!("Row: {} is full", row);
                 score += 1;
                 self.remove_row(row);
             }
@@ -59,12 +58,12 @@ impl ExtendedGrid {
     }
     pub fn blocks(&self) -> [Block; (HEIGHT+3) * (WIDTH+2*3)] {
         let mut blocks = [Block {
-            position: Position::new_abs(0, 0),
+            position: Position::new(0, 0),
             colour: YELLOW,
         }; (HEIGHT+3) * (WIDTH+2*3)];
         for x in 0..WIDTH+6 {
             for y in 0..HEIGHT {
-                blocks[x+y*(WIDTH+2*3)].position = Position::new_abs(x as i8, y as i8);
+                blocks[x+y*(WIDTH+2*3)].position = Position::new(x as i8, y as i8);
                 // TODO: Why subtract by 2 not 3?
                 if self.0[x][y] == 0xFF && x > 2 && x < WIDTH+6 && y < HEIGHT {
                     blocks[x+y*(WIDTH+2*3)].colour = GREEN;
@@ -99,7 +98,7 @@ impl <'a>Iterator for ExtendedGridIterator<'a> {
                     self.y += 1;
                     //println!("Woah");
                     return Some(Block {
-                        position: Position::new_abs(self.x as i8, self.y as i8),
+                        position: Position::new(self.x as i8, self.y as i8),
                         colour: GREEN,
                     });
                 }
@@ -202,13 +201,13 @@ impl Piece {
         Self {
             shape: rng.gen_range(0..2),
             rotation: [Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT][rng.gen_range(0..4)],
-            pos: Position::new_abs(rng.gen_range(3..(WIDTH) as i8), 0),
+            pos: Position::new(rng.gen_range(3..(WIDTH) as i8), 0),
         }
     }
     pub fn blocks(&self) -> [Block; 16] {
         let ((off_x, off_y), shape) = PIECES[self.shape][self.rotation as usize];
         let mut blocks = [Block {
-            position: Position::new_abs(0, 0),
+            position: Position::new(0, 0),
             colour: YELLOW,
         }; 16];
 
@@ -217,7 +216,7 @@ impl Piece {
                 if shape[y][x] == 1 {
                     let res_x = (self.pos.x as i8) + (x as i8);
                     let res_y = (self.pos.y as i8) + (y as i8);
-                    blocks[x+y*4].position = Position::new_abs(res_x, res_y);
+                    blocks[x+y*4].position = Position::new(res_x, res_y);
                     //if res_x >= 3 && res_x < 3 + 9 && res_y < 34+3 {
                         blocks[x+y*4].colour = GREEN;
                     //}
@@ -236,21 +235,8 @@ pub struct Position {
 }
 
 impl Position {
-    fn new() -> Position {
-        Position {
-            x: BOARD_WIDTH / 2,
-            y: BOARD_HEIGHT / 2,
-        }
-    }
-
-    fn new_abs(x: i8, y: i8) -> Position {
+    fn new(x: i8, y: i8) -> Position {
         Position { x: x as u8, y: y as u8 }
-    }
-
-    fn new_offset(x: i8, y: i8) -> Position {
-        let mut pos = Position::new();
-        pos.offset(x, y);
-        pos
     }
 
     fn offset(&mut self, x: i8, y: i8) {
@@ -302,14 +288,6 @@ pub enum Direction {
 }
 
 impl Direction {
-    fn opposite(&self) -> Self {
-        match self {
-            Direction::UP => Direction::DOWN,
-            Direction::DOWN => Direction::UP,
-            Direction::LEFT => Direction::RIGHT,
-            Direction::RIGHT => Direction::LEFT,
-        }
-    }
     fn next(&self) -> Self {
         match self {
             Direction::UP => Direction::RIGHT,
@@ -320,101 +298,7 @@ impl Direction {
     }
 }
 
-pub struct Snake {
-    pub body: VecDeque<Block>,
-    pub direction: Direction,
-    pub alive: bool,
-    pub eat: bool,
-}
-
-impl Snake {
-    fn new() -> Snake {
-        Snake {
-            body: VecDeque::from(vec![
-                Block {
-                    position: Position::new(),
-                    colour: YELLOW,
-                },
-                Block {
-                    position: Position::new_offset(-1, 0),
-                    colour: GREEN,
-                },
-                Block {
-                    position: Position::new_offset(-2, 0),
-                    colour: GREEN,
-                },
-            ]),
-            direction: Direction::UP,
-            alive: true,
-            eat: false,
-        }
-    }
-
-    fn update(&mut self, mut dir: Direction) {
-        if self.direction == dir.opposite() {
-            // Do nothing
-        } else {
-            self.direction = dir;
-        }
-    }
-
-    fn perform_next(&mut self, food_pos: &mut Position) {
-        if self.alive {
-            let next_pos = self.next_head_pos();
-            if self.check_collide_wall(next_pos) || self.check_collide_body(next_pos) {
-                self.alive = false;
-            } else if self.check_eat_food(next_pos, *food_pos) {
-                self.eat_next(food_pos);
-                self.eat = true;
-            } else {
-                self.move_next();
-            }
-        }
-    }
-
-    fn next_head_pos(&mut self) -> Position {
-        let mut current_head = self.body[0].position;
-        match self.direction {
-            Direction::RIGHT => current_head.offset(1, 0),
-            Direction::UP => current_head.offset(0, -1),
-            Direction::LEFT => current_head.offset(-1, 0),
-            Direction::DOWN => current_head.offset(0, 1),
-        }
-        current_head
-    }
-
-    fn check_collide_wall(&self, next_pos: Position) -> bool {
-        self.body[0].position == next_pos
-    }
-
-    fn check_collide_body(&self, pos: Position) -> bool {
-        self.body.iter().any(|block| block.position == pos)
-    }
-
-    fn check_eat_food(&self, next_pos: Position, food_pos: Position) -> bool {
-        next_pos == food_pos
-    }
-
-    fn move_next(&mut self) {
-        for i in (1..self.body.len()).rev() {
-            self.body[i].position = self.body[i - 1].position;
-        }
-        self.body[0].position = self.next_head_pos();
-    }
-
-    fn eat_next(&mut self, pos: &mut Position) {
-        let head = Block {
-            position: *pos,
-            colour: YELLOW,
-        };
-        self.body.push_front(head);
-        self.body[1].colour = GREEN;
-    }
-}
-
 pub struct Game {
-    pub snake: Snake,
-    pub food: Block,
     pub time: u32,
     pub score: u32,
     pub board: ExtendedGrid,
@@ -425,11 +309,6 @@ pub struct Game {
 impl Game {
     pub fn new() -> Game {
         Game {
-            snake: Snake::new(),
-            food: Block {
-                position: Position::new(),
-                colour: RED,
-            },
             time: 0,
             score: 0,
             board: ExtendedGrid::default(),
@@ -439,8 +318,6 @@ impl Game {
     }
 
     pub fn init(&mut self) {
-        self.snake = Snake::new();
-        self.food.position = self.get_food_pos();
         self.time = 0;
         self.score = 0;
 
@@ -464,7 +341,7 @@ impl Game {
             if dir == Direction::DOWN {
                 self.fallen_down();
             }
-            println!("Collision");
+            //println!("Collision");
         } else {
             self.piece = next_piece;
         };
@@ -493,10 +370,10 @@ impl Game {
 
     fn fallen_down(&mut self) {
         self.save();
-        println!("Rock bottom");
+        //println!("Rock bottom");
 
-        let score = self.board.compact_rows();
-        println!("Score: {}", score);
+        self.score = self.board.compact_rows();
+        println!("Score: {}", self.score);
 
         self.piece = Piece::random();
         // If the newly generated piece already overlaps, it's game over
@@ -520,52 +397,16 @@ impl Game {
         } else {
             self.piece = next_piece;
         };
-
-        if self.snake.alive {
-            self.snake.perform_next(&mut self.food.position);
-            self.time += 1;
-            if self.snake.eat {
-                self.score += 1;
-                self.food.position = self.get_food_pos();
-                self.snake.eat = false;
-            }
-        }
     }
 
-    fn get_direction_from_index(&self, index: usize) -> Direction {
+    // TODO: Do I need this?
+    fn _get_direction_from_index(&self, index: usize) -> Direction {
         match index {
             0 => Direction::RIGHT,
             1 => Direction::UP,
             2 => Direction::LEFT,
             3 => Direction::DOWN,
-            _ => self.snake.direction,
-        }
-    }
-
-    fn get_food_pos(&mut self) -> Position {
-        let mut rng = rand::thread_rng();
-        loop {
-            let pos = Position {
-                x: rng.gen_range(0..BOARD_WIDTH),
-                y: rng.gen_range(0..BOARD_HEIGHT),
-            };
-            if !self.snake.check_collide_body(pos) {
-                return pos;
-            }
-        }
-    }
-
-    fn get_food_dist(&self) -> i64 {
-        let dist_x = (self.snake.body[0].position.x as i64 - self.food.position.x as i64).abs();
-        let dist_y = (self.snake.body[0].position.y as i64 - self.food.position.y as i64).abs();
-        dist_x + dist_y
-    }
-
-    fn get_pos_dead(&self, pos: Position) -> f64 {
-        if self.snake.check_collide_wall(pos) || self.snake.check_collide_body(pos) {
-            1f64
-        } else {
-            0f64
+            _ => unimplemented!(),
         }
     }
 }
